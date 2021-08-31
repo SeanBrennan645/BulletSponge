c35a976692942cec7c73bf6ee68585ea33025e79
SeanBrennan645 <seanbrennan645@gmail.com>
2021-08-31 18:48:43 +0100

    First Commit

---
+++ BulletSponge/Source/BulletSponge/BulletSpongeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BulletSpongeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BULLETSPONGE_API ABulletSpongeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
