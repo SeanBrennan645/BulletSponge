430dcbcc1016ddf413a03dfe9758e8879e27dcf7
SeanBrennan645 <seanbrennan645@gmail.com>
2021-09-01 17:50:15 +0100

    Began work on movement
    There is currently an issue with looking up and down which needs to be worked on

---
+++ BulletSponge/Source/BulletSponge/Player/PlayerCharacter.cpp
@@ -0,0 +1,61 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerCharacter.h"
+
+// Sets default values
+APlayerCharacter::APlayerCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void APlayerCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APlayerCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis(TEXT("MoveFwdBwd"), this, &APlayerCharacter::MoveFwdBwd);
+	PlayerInputComponent->BindAxis(TEXT("MoveSideways"), this, &APlayerCharacter::MoveSideways);
+	PlayerInputComponent->BindAxis(TEXT("LookUpDown"), this, &APawn::AddControllerPitchInput);
+	PlayerInputComponent->BindAxis(TEXT("LookSideways"), this, &APawn::AddControllerYawInput);
+	PlayerInputComponent->BindAxis(TEXT("LookUpDownRate"), this, &APlayerCharacter::LookUpDownRate);
+	PlayerInputComponent->BindAxis(TEXT("LookSidewaysRate"), this, &APlayerCharacter::LookSidewaysRate);
+
+}
+
+//MovementFunctions
+void APlayerCharacter::MoveFwdBwd(float AxisValue)
+{
+	AddMovementInput(GetActorForwardVector() * AxisValue);
+}
+
+void APlayerCharacter::MoveSideways(float AxisValue)
+{
+	AddMovementInput(GetActorRightVector() * AxisValue);
+}
+
+void APlayerCharacter::LookUpDownRate(float AxisValue)
+{
+	AddControllerPitchInput(AxisValue * RotationRate * GetWorld()->GetDeltaSeconds());
+}
+
+void APlayerCharacter::LookSidewaysRate(float AxisValue)
+{
+	AddControllerYawInput(AxisValue * RotationRate * GetWorld()->GetDeltaSeconds());
+}
---
+++ BulletSponge/Source/BulletSponge/Player/PlayerCharacter.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "PlayerCharacter.generated.h"
+
+UCLASS()
+class BULLETSPONGE_API APlayerCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	APlayerCharacter();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+	void MoveFwdBwd(float AxisValue);
+	void MoveSideways(float AxisValue);
+	void LookUpDownRate(float AxisValue);
+	void LookSidewaysRate(float AxisValue);
+
+	UPROPERTY(EditAnywhere)
+		float RotationRate = 20.0f;
+	
+
+};
